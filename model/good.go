66f65fec7f8d0c14f157acf259034987481541b7
helmimuzkr <mhelmimuzakir@gmail.com>
2023-01-05 00:30:49 +0700

    add: model good

---
+++ model/good.go
@@ -0,0 +1,42 @@
+package model
+
+import "gorm.io/gorm"
+
+type Good struct {
+	gorm.Model
+	UserID int    `json:"user_id"`
+	User   User   `gorm:"constraint"`
+	Name   string `json:"name" form:"nama"`
+	Qty    string `json:"qty" form:"qty"`
+}
+
+type GoodModel struct {
+	DB *gorm.DB
+}
+
+func (gm *GoodModel) Insert(good Good) (Good, error) {
+	tx := gm.DB.Create(&good)
+	if tx.Error != nil {
+		return Good{}, tx.Error
+	}
+
+	return good, nil
+}
+
+func (gm *GoodModel) GetAll(userID int) ([]Good, error) {
+	goods := []Good{}
+	tx := gm.DB.
+		Where("goods.user_id = ?", userID).
+		Joins("JOIN users ON users.id = goods.id").
+		Find(&goods)
+	if tx.Error != nil {
+		return nil, tx.Error
+	}
+
+	return goods, nil
+}
+
+// func (gm *GoodModel) GetByID(goodID int, userID int) (Good, error) {
+// 	good := Good{}
+// 	tx := gm.DB.Where()
+// }
