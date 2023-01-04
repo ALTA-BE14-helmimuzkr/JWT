d8678497cee292509bbd95604fdd6a99e870fbf5
helmimuzkr <mhelmimuzakir@gmail.com>
2023-01-05 01:37:36 +0700

    update: GetById, Update, and Delete method

--- controller/good.go
+++ controller/good.go
@@ -4,6 +4,7 @@ import (
 	"api/model"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -13,6 +14,7 @@ type GoodController struct {
 	JWTKey string
 }
 
+// Menambahkan barang baru
 func (gc *GoodController) Create() echo.HandlerFunc {
 	return func(c echo.Context) error {
 		good := model.Good{}
@@ -20,7 +22,7 @@ func (gc *GoodController) Create() echo.HandlerFunc {
 			return c.JSON(http.StatusBadRequest, "format inputan salah")
 		}
 
-		userID := ExtractToken(c)
+		userID := ExtractToken(c) // id pemilik dari proses extract token
 		good.UserID = userID
 
 		good, err := gc.Mdl.Insert(good)
@@ -36,9 +38,10 @@ func (gc *GoodController) Create() echo.HandlerFunc {
 	}
 }
 
+// Mendapatkan barang yang dipunyai oleh pemilik
 func (gc *GoodController) GetAll() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		userID := ExtractToken(c)
+		userID := ExtractToken(c) // id pemilik dari proses extract token
 		goods, err := gc.Mdl.GetAll(userID)
 		if err != nil {
 			log.Println("query error", err.Error())
@@ -47,6 +50,79 @@ func (gc *GoodController) GetAll() echo.HandlerFunc {
 
 		return c.JSON(http.StatusOK, map[string]interface{}{
 			"data":    goods,
-			"message": "sukses mendapatkan semua data"})
+			"message": "sukses mendapatkan semua data",
+		})
+	}
+}
+
+// Mendapatkan barang punya pemilik yang dipilih menggunakan id barang
+func (gc *GoodController) GetByID() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		userID := ExtractToken(c)                  // id pemilik dari proses extract token
+		goodID, err := strconv.Atoi(c.Param("id")) // id barang dari uri parameter
+		if err != nil {
+			log.Println("convert id error ", err.Error())
+			return c.JSON(http.StatusBadRequest, map[string]interface{}{
+				"message": "gunakan input angka",
+			})
+		}
+
+		good, err := gc.Mdl.GetByID(userID, goodID)
+		if err != nil {
+			log.Println("query error", err.Error())
+			return c.JSON(http.StatusInternalServerError, "tidak bisa diproses")
+		}
+
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"data":    good,
+			"message": "sukses mendapatkan semua data",
+		})
+	}
+}
+
+// Mendapatkan barang punya pemilik yang dipilih menggunakan id barang
+func (gc *GoodController) Update() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		good := model.Good{}
+		if err := c.Bind(&good); err != nil {
+			return c.JSON(http.StatusBadRequest, "format inputan salah")
+		}
+
+		userID := ExtractToken(c) // id pemilik dari proses extract token
+		good.UserID = userID
+
+		good, err := gc.Mdl.Update(good)
+		if err != nil {
+			log.Println("query error", err.Error())
+			return c.JSON(http.StatusInternalServerError, "tidak bisa diproses")
+		}
+
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"data":    good,
+			"message": "berhasil update data",
+		})
+	}
+}
+
+func (gc *GoodController) Delete() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		userID := ExtractToken(c)                  // id pemilik dari token
+		goodID, err := strconv.Atoi(c.Param("id")) // id barang dari uri parameter
+		if err != nil {
+			log.Println("convert id error ", err.Error())
+			return c.JSON(http.StatusBadRequest, map[string]interface{}{
+				"message": "gunakan input angka",
+			})
+		}
+
+		err = gc.Mdl.Delete(userID, goodID)
+		if err != nil {
+			log.Println("query error", err.Error())
+			return c.JSON(http.StatusInternalServerError, "tidak bisa diproses")
+		}
+
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"message": "sukses menghapus data",
+		})
 	}
 }
